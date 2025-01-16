8f49c184114e0f5f6aaeadd76abf761a5485d267
MadeSatya5 <madesatya505@gmail.com>
2025-01-16 22:10:15 +0700

    feat: add friend

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Button from "./components/Button";
+import FormAddFriend from "./components/FormAddFriend";
+import FriendList from "./components/FriendsList";
+
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [friends, setFriends] = useState(initialFriends);
+  const [openAddFriend, setOpenAddFriend] = useState(false);
+
+  function handleAddFriend(friend){
+    setFriends((friends) => [...friends, friend])
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="app">
+        <div className="sidebar">
+          <FriendList friends={friends}/>
+          {openAddFriend && <FormAddFriend onAddFriend={handleAddFriend}/>}
+          <Button onClick={() => setOpenAddFriend((prev) => !prev)}>
+            {openAddFriend ? "Close" : "Add Friend"}
+          </Button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
